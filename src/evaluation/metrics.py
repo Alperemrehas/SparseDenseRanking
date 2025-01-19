6799841f100e67d82b8d19a7e8702121e48eb5ad
unknown <cihadtekinbas@gmail.com>
2025-01-19 21:20:33 +0300

    refactor code

--- src/evaluation/metrics.py
+++
@@ -1,7 +0,0 @@
-from sklearn.metrics import ndcg_score, precision_score, recall_score
-
-def evaluate(predictions, ground_truths):
-    ndcg = ndcg_score([ground_truths], [predictions])
-    precision = precision_score(ground_truths, predictions, average='macro')
-    recall = recall_score(ground_truths, predictions, average='macro')
-    return {'NDCG': ndcg, 'Precision': precision, 'Recall': recall}
--- src/indexing/sparse_indexing.py
+++
@@ -1,7 +0,0 @@
-from pyserini.index import SimpleIndexer
-
-def create_sparse_index(data_dir, index_dir):
-    indexer = SimpleIndexer(data_dir, index_dir)
-    indexer.set_analyzer('standard')  # Use a standard analyzer
-    indexer.index()
-    print(f"Sparse index created at {index_dir}")
